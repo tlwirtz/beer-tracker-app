9e7962115fa38495ddcff5d7839e85bfba2e18f4
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2016-09-03 18:02:02 -0700

    very basic component and store set up. React rendering properly

--- source/entry.js
+++ source/entry.js
@@ -1,1 +1,18 @@
-console.log('i\'m alive');
+'use strict';
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import counter from './reducers'
+import App from './components/App'
+
+const store = createStore(counter)
+const rootEl = document.getElementById('app')
+
+function render() {
+  ReactDOM.render(
+    <App />, rootEl
+  )
+}
+
+render()
