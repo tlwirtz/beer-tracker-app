da54702df38698324dfdad3b44b2a5be383e9925
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2016-10-01 13:23:09 -0700

    set up async action creators

--- client/actions/actionCreators.js
+++ client/actions/actionCreators.js
@@ -1,6 +1,31 @@
+export const ADD_BEER = 'ADD_BEER'
+export const FETCH_BEER_LIST = 'FETCH_BEER_LIST'
+export const FETCH_BEER_LIST_SUCCESS = 'FETCH_BEER_LIST_SUCCESS'
+export const FETCH_BEER_LIST_FAILURE = 'FETCH_BEER_LIST_FAILURE'
+
 export const addBeer = (beer) => (
   {
-    type: 'ADD_BEER',
+    type: ADD_BEER,
     beer
+  }
+)
+
+export const fetchBeerList = () => (
+  {
+    type: FETCH_BEER_LIST
+  }
+)
+
+export const fetchBeerListSuccess = (res) => (
+  {
+    type: FETCH_BEER_LIST_SUCCESS,
+    beers: res.body
+  }
+)
+
+export const fetchBeerListFailure = (err) => (
+  {
+    type: FETCH_BEER_LIST_FAILURE,
+    error: err
   }
 )
