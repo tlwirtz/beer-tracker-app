c8922f215ccedef86b1d149ae3954930ef508124
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2016-10-04 18:01:25 -0700

    action creators for inventory transactions

--- client/actions/actionCreators.js
+++ client/actions/actionCreators.js
@@ -6,6 +6,34 @@ export const SELECT_BEER = 'SELECT_BEER'
 export const FETCH_BEER_LIST = 'FETCH_BEER_LIST'
 export const FETCH_BEER_LIST_SUCCESS = 'FETCH_BEER_LIST_SUCCESS'
 export const FETCH_BEER_LIST_FAILURE = 'FETCH_BEER_LIST_FAILURE'
+export const ADD_INVENTORY_TRANSACTION = 'ADD_INVENTORY_TRANSACTION'
+export const ADD_INVENTORY_TRANSACTION_FAILURE = 'ADD_INVENTORY_TRANSACTION_FAILURE'
+export const ADD_INVENTORY_TRANSACTION_SUCCESS = 'ADD_INVENTORY_TRANSACTION_SUCCESS'
+
+export const addInventoryTrans = (beer, qty, transType) => (
+  {
+    type: ADD_INVENTORY_TRANSACTION,
+    beer,
+    qty,
+    transType
+  }
+)
+
+export const addInventoryTransSuccess = (beerId, transactions) => (
+  {
+    type: ADD_INVENTORY_TRANSACTION_SUCCESS,
+    beerId,
+    transactions
+  }
+)
+
+export const addInventoryTransFail = (beerId, err) => (
+  {
+    type: ADD_INVENTORY_TRANSACTION_FAILURE,
+    beerId,
+    err
+  }
+)
 
 export const selectBeer = (beer) => (
   {
@@ -42,6 +70,22 @@ export const fetchBeerListFailure = (err) => (
   }
 )
 
+export const sendInventoryTrans = (beer, qty) => (
+  (dispatch) => {
+    const body = {
+      type: qty >= 0 ? 'adjust-up' : 'adjust-down',
+      qty
+    }
+
+    dispatch(addInventoryTrans())
+
+    //how do you post with `fetch`
+    return fetch(`${process.env.BEER_TRACKER_API}/api/beer${beer._id}/transaction`)
+    .then((response) => response.json())
+    .then((newBeer) => dispatch(addInventoryTransSuccess(newBeer._id, newBeer.transactions)))
+    .catch((err) => dispatch(addInventoryTransFail(beer._id, err)))
+  }
+)
 export const fetchBeers = () => (
   (dispatch) => {
     dispatch(fetchBeerList())
