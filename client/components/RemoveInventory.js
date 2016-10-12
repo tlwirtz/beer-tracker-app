835e4117d40188ca122dc650256a2b1fc87ea4ed
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2016-10-11 19:21:40 -0700

    add and remove components

---
+++ client/components/RemoveInventory.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import classNames from 'classnames'
+
+class RemoveInventory extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick() {
+    this.props.sendInventoryTrans(this.props.beer, -1)
+  }
+
+  render() {
+    let classes = classNames({
+      'beerSingle-button': true
+    })
+
+    return (
+      <div className={classes}>
+        <button key={this.props.beer._id}
+          onClick={this.handleClick}>
+            -
+          </button>
+      </div>
+    )
+  }
+}
+ export default RemoveInventory
