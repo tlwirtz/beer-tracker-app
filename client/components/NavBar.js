78411299b1e81d43cd078f79f281d7305c98267c
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2016-10-15 14:57:42 -0700

    add Nav Bar

---
+++ client/components/NavBar.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import classNames from 'classnames'
+
+class NavBar extends React.Component {
+  constructor(props){
+    super(props)
+  }
+
+  render() {
+
+    const navLeft = classNames({
+      'nav-left':true,
+    })
+
+    const nav = classNames({
+      'nav':true,
+      'has-shadow': true,
+      'beerTrackerNav': true
+    })
+
+    const navRight = classNames({
+      'nav-right':true
+    })
+
+    const navItem = classNames({
+      'is-tab': true,
+      'nav-item': true
+    })
+
+    const navLogo = classNames({
+      'nav-item': true,
+      'is-brand': true,
+      'title': true,
+      'is-2': true
+    })
+
+    return(
+      <nav className={nav}>
+        <div className={navLeft}>
+          <h1 className={navLogo}>Beer Tracker</h1>
+        </div>
+        <div className={navRight}>
+          <h2 className={navItem}>Add New Beer</h2>
+        </div>
+      </nav>
+    )
+  }
+}
+
+export default NavBar
