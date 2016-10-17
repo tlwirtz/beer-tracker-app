0442bafe301135511a5fded13128d49a7dd5f377
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2016-10-16 18:21:40 -0700

    add skeleton beer and device management pages

---
+++ client/components/ManageBeers.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class ManageBeers extends React.Component {
+    constructor(props) {
+      super(props)
+    }
+
+    render() {
+        return (
+            <h1> MANAGING BEERS</h1>
+        )
+    }
+}
+
+export default ManageBeers
