9be712ee8e834602d17ea19c0ad2cd73e46d44ee
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2016-10-25 20:18:06 -0700

    beer transactions

--- client/components/BeerTransaction.js
+++ client/components/BeerTransaction.js
@@ -2,30 +2,46 @@ import React from 'react'
 import classNames from 'classnames'
 
 const BeerTransaction = (props) => {
-  const subtitle = classNames('subtitle', 'is-5', 'level-item')
   const options = { weekday: 'short', year: 'numeric', month: 'long', day: 'numeric' };
+  const transDate = new Date(props.transaction.dateTime).toLocaleDateString('en-US', options)
   const level = classNames('level', 'is-mobile')
+  const heading = classNames('heading')
+  const subtitle = classNames('subtitle')
   const levelItem = classNames('level-item')
-  const levelLeft = classNames('level-left')
-  const levelRight = classNames('level-right')
+  const levelItemCentered = classNames('level-item', 'has-text-centered')
+  const levelRight = classNames('level-right', 'has-text-centered')
   const icon = classNames('icon')
   const arrow = classNames({
     'fa': true,
     'fa-arrow-up': props.transaction.type === 'adjust-up',
-    'fa-arrow-down': props.transaction.type === 'adjust-down'
+    'beer-tracker-green': props.transaction.type === 'adjust-up',
+    'fa-arrow-down': props.transaction.type === 'adjust-down',
+    'beer-tracker-red': props.transaction.type === 'adjust-down'
   })
+
   return (
+    <div>
     <div className={level}>
-      <div className={levelLeft}>
-        <p className={subtitle}>{new Date(props.transaction.dateTime).toLocaleDateString('en-US', options)}</p>
+      <div className={levelItem}>
+        <p className={heading}>date</p><br />
+        <p className={subtitle}>{transDate}</p>
+      </div>
+      <div className={levelItemCentered}>
+        <p className={heading}>qty</p><br />
+        <p className={subtitle}>{props.transaction.qty}</p>
+      </div>
+      <div className={levelItemCentered}>
+        <p className={heading}>type</p><br />
+        <p className={subtitle}>{props.transaction.type}</p>
       </div>
       <div className={levelRight}>
-        <div className={levelItem}>
-          <span className={icon}>
+        <p className={subtitle}>
+        <span className={icon}>
             <i className={arrow}></i>
-          </span>
-        </div>
+          </span></p>
       </div>
+    </div>
+    <hr />
     </div>
   )
 }
