97cee2cd1239c08612976e2cfe591a9a2f4418b3
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2016-11-07 20:00:54 -0800

    update detail with graph placeholders and new inventory transaction list

--- client/components/BeerDetailContainer.js
+++ client/components/BeerDetailContainer.js
@@ -2,30 +2,46 @@ import React from 'react'
 import classNames from 'classnames'
 import BeerDetailHeader from './BeerDetailHeader'
 import BeerTransactionList from './BeerTransactionList'
-
+import Inventory from './Inventory'
+import GraphContainer from './GraphContainer'
 
 class BeerDetailContainer extends React.Component {
   constructor(props) {
     super(props)
 
     this.filterBeerItems.bind(this)
+    this.calculateInventory.bind(this)
   }
 
   filterBeerItems() {
     return this.props.beers.items.filter((beer) => beer._id === this.props.params.beerId)[0]
   }
 
+  calculateInventory() {
+    const beer = this.filterBeerItems();
+    if (beer.transactions.length === 0 ) return 0
+    return beer.transactions
+      .map(((trans) => trans.type === 'adjust-up' ? trans.qty : -(trans.qty)))
+      .reduce((a, b) => a + b, 0)
+  }
+
   render() {
-    const underConstruction = classNames('title', 'is-large', 'has-text-centered', 'notification', 'is-warning')
+    const flex = classNames('flex-container')
+    const headerContainer = classNames('flex-detail-header-main')
     return (
       <div>
         { this.props.beers.fetchingBeers ?
-            <h1>FETCHING BEER</h1> :
-            <div>
-              <h1 className={underConstruction}>UNDER CONSTRUCTION</h1>
-              <BeerDetailHeader beer={this.filterBeerItems()} />
-              <BeerTransactionList transactions={this.filterBeerItems().transactions} />
+          <h1>FETCHING BEER</h1> :
+          <div>
+            <div className={flex}>
+              <div className={headerContainer}>
+                <BeerDetailHeader beer={this.filterBeerItems()} />
+              </div>
+              <Inventory detailPage={true} qty={this.calculateInventory()} />
             </div>
+            <GraphContainer />
+            <BeerTransactionList transactions={this.filterBeerItems().transactions} />
+          </div>
         }
       </div>
     )
