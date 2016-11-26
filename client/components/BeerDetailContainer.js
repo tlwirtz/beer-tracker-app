47510f72bfad9dc2a9e610d048ff2e2b405abf74
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2016-11-26 11:57:55 -0800

    made functional, rather than imperative, not sure if it is any better...

--- client/components/BeerDetailContainer.js
+++ client/components/BeerDetailContainer.js
@@ -8,7 +8,7 @@ import AddInventory from './AddInventory'
 import RemoveInventory from './RemoveInventory'
 import BeerGraph from './BeerGraph'
 
-import * as _ from 'ramda'
+import * as R from 'ramda'
 
 class BeerDetailContainer extends React.Component {
   constructor(props) {
@@ -32,36 +32,68 @@ class BeerDetailContainer extends React.Component {
   }
 
   formatForGraph() {
-    //THIS IS SUPER UGLY -- FUNCTIONALIZE!
     const beer = this.filterBeerItems();
-    const posOrNeg = _.curry((type, qty) => type === 'adjust-up' ? qty : -(qty))
-    const formatDate = _.curry((options, date) => new Date(date).toLocaleDateString('en-US', options))
 
-    const totalDailyTrans = (dailyTrans, trans) => {
-      const options = { year: 'numeric', month: 'long', day: 'numeric' };
-      const shortDate = formatDate(options)
-      const transDate = shortDate(trans.dateTime)
-      const checkQty = posOrNeg(trans.type)
-      dailyTrans[transDate] ? dailyTrans[transDate] += checkQty(trans.qty) : dailyTrans[transDate] = checkQty(trans.qty)
-      return dailyTrans
+  /**
+   * TODO -- most of thi should go into a helper library.
+   * This is a lot of functional programming...
+   * Is this good practice? Could be hell to maintain later...
+   */
+
+    //const format date
+    const formatDate = R.curry((options, date) => new Date(date).toLocaleDateString('en-US', options))
+    const options = { year: 'numeric', month: 'long', day: 'numeric'}
+    const transformDate = { dateTime: formatDate(options) }
+
+    //const sort by dates
+    const sortByDate = R.sortBy(R.prop('dateTime'))
+    const groupByDate = R.groupBy(R.prop('dateTime'))
+
+    // filter out the adjust downs and adjust up
+    // might also consider a `group by`
+    const filterAdjDown = R.propEq('type', 'adjust-down')
+    const filterAdjUp = R.propEq('type', 'adjust-up')
+    const adjDown = R.compose(R.map(R.evolve({ qty: R.negate })), R.filter(filterAdjDown))
+    const adjUp = R.filter(filterAdjUp)
+    const transformQty = (list) => R.concat(adjDown(list), adjUp(list))
+
+    //negate adjust down, sort by date, transform to short date
+    const sanitizeTransactions =  R.compose(
+      R.map(R.evolve(transformDate)),
+      sortByDate,
+      transformQty
+    )
+
+    // transform into the final format we require for the graph
+    const reduceQty = (accum, obj) => R.add(accum, R.prop('qty', obj))
+    const calcDailyTransactions = R.map(R.reduce(reduceQty, 0))
+    const dailyInventory = R.compose(R.drop(1), R.scan(R.add, 0), R.values)
+    const mergeDailyInventory = R.compose(R.zipObj, R.keys)
+
+    //sanitize, group by date, add up transaction qtys
+    const sanitizeAndCalc = R.compose(
+      calcDailyTransactions,
+      groupByDate,
+      sanitizeTransactions
+    )
+
+    //calculate how much was in inventory each day
+    const dailyInvTotal = (transactions) => {
+      return R.compose(
+        mergeDailyInventory(transactions),
+        dailyInventory,
+      )(transactions)
     }
 
-    const dailyTransactions = beer.transactions.reduce(totalDailyTrans, {})
+    const finalInventory =  R.compose(
+      R.values,
+      R.mapObjIndexed((value, key, obj) => ({date: key,  qty: value})),
+      dailyInvTotal,
+      sanitizeAndCalc
+    )
 
-    Object.keys(dailyTransactions).reduce((inventory, key) => {
-      inventory += dailyTransactions[key]
-      dailyTransactions[key] = inventory
-      return inventory
-    }, 0)
-
-    const finalInventory = Object.keys(dailyTransactions).reduce((inventory, key) => {
-        console.log('inventory', inventory)
-        inventory.push({date: key, qty: dailyTransactions[key]})
-        return inventory
-    }, [])
-
-    console.log(finalInventory)
-    return finalInventory
+    console.log(finalInventory(beer.transactions))
+    return finalInventory(beer.transactions)
   }
 
   render() {
